9da249faaa0d8f50c0d51487decde93b99ebad14
Zeneil Ambekar <zeneil@getfocus.in>
2015-07-31 21:30:58 +0530

    Added more util functionality

--- CoreUtils/src/main/java/com/zva/android/commonLib/utils/ClassLoaderUtils.java
+++ CoreUtils/src/main/java/com/zva/android/commonLib/utils/ClassLoaderUtils.java
@@ -1,8 +1,64 @@
 package com.zva.android.commonLib.utils;
 
+import android.content.Context;
+import dalvik.system.DexFile;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Copyright CoreStorage 2015
- * Created by zeneilambekar on 31/07/15.
+ * Copyright CoreStorage 2015 Created by zeneilambekar on 31/07/15.
  */
 public class ClassLoaderUtils {
+
+    /**
+     * Finds all the set of classes located in given package names
+     *
+     * @param scannedPackages the packages to be scanned
+     * @param applicationContext the application context
+     * @return the set of classes located in the scanned packages
+     * @throws IOException if searching the dex file throws any IOException
+     */
+    public <T> Set<Class<? extends T>> find(String[] scannedPackages, Context applicationContext) throws IOException {
+        return find(scannedPackages, applicationContext, null);
+    }
+
+    /**
+     * Finds the set of classes located in given package names having a particular annotation
+     *
+     * @param scannedPackages the packages to be scanned
+     * @param applicationContext the application context
+     * @param annotationClassToSearchFor the annotation class to search for
+     * @return the set
+     * @throws IOException if searching the dex file throws any IOException
+     */
+    public <T> Set<Class<? extends T>> find(String[] scannedPackages, Context applicationContext, Class<? extends Annotation> annotationClassToSearchFor) throws IOException {
+        Enumeration<String> entries = new DexFile(applicationContext.getPackageCodePath()).entries();
+
+        Set<Class<? extends T>> returnClassSet = new HashSet<>();
+
+        while (entries.hasMoreElements()) {
+            String potentialPackageClassName = entries.nextElement();
+
+            for (String scannedPackage : scannedPackages) {
+                if (potentialPackageClassName.startsWith(scannedPackage)) {
+                    try {
+                        @SuppressWarnings("unchecked")
+                        //If there is a class cast exception, treat the class as malformed and move on
+                        Class<? extends T> potentialPackageClass = (Class<? extends T>) Class.forName(potentialPackageClassName);
+                        if (annotationClassToSearchFor == null || potentialPackageClass.getAnnotation(annotationClassToSearchFor) != null)
+                            returnClassSet.add(potentialPackageClass);
+                    } catch (ClassNotFoundException | ClassCastException ignored) {
+                    }
+                    break;
+                }
+            }
+        }
+        return returnClassSet;
+
+    }
+
 }
--- CoreUtils/src/main/java/com/zva/android/commonLib/utils/PackageUtils.java
+++ CoreUtils/src/main/java/com/zva/android/commonLib/utils/PackageUtils.java
@@ -1,8 +1,93 @@
 package com.zva.android.commonLib.utils;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.sun.istack.internal.Nullable;
+
 /**
- * Copyright CoreStorage 2015
- * Created by zeneilambekar on 31/07/15.
+ * Copyright CoreStorage 2015 Created by zeneilambekar on 31/07/15.
  */
 public class PackageUtils {
+
+    /**
+     * Gets application meta data.
+     *
+     * @param applicationContext the application context
+     * @return the application meta data
+     */
+    @Contract(value = "null -> null", pure = true)
+    public static Bundle getApplicationMetaData(@NotNull Context applicationContext) {
+
+        Bundle metaData = null;
+
+        try {
+
+            metaData = applicationContext.getPackageManager().getApplicationInfo(applicationContext.getPackageName(), PackageManager.GET_META_DATA).metaData;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+
+        return metaData;
+    }
+
+    /**
+     * Checks if the specified permissions have been acquired by the current android app
+     *
+     * @param applicationContext the application context
+     * @param requiredPermissionNames the required permission names
+     */
+    public static void checkPermissions(@NotNull Context applicationContext, @NotNull Permissions... requiredPermissionNames) {
+
+        for (Permissions permissionName : requiredPermissionNames) {
+
+            applicationContext.enforceCallingOrSelfPermission(permissionName.getName(), String.format("Missing permission in manifest: '%s'", permissionName));
+
+        }
+
+    }
+
+    /**
+     * Gets application meta data given a metaData key
+     *
+     * @param applicationContext the application context
+     * @param metaDataKey the meta data key
+     * @param defaultValue the default value
+     * @return the application meta data
+     */
+    @Contract(value = "_, _, !null -> !null", pure = true)
+    public static <T> T getApplicationMetaData(@NotNull Context applicationContext, @NotNull String metaDataKey, @Nullable T defaultValue) throws ClassCastException {
+
+        Bundle metaData = getApplicationMetaData(applicationContext);
+
+        if (metaData != null)
+            //noinspection unchecked
+            return (T) metaData.get(metaDataKey);
+
+        return defaultValue;
+    }
+
+    /**
+     * The enum containing a list of android permissions
+     */
+    public enum Permissions {
+
+        ACCESS_NETWORK_STATE("android.permission.ACCESS_NETWORK_STATE"), ACCESS_COARSE_LOCATION("android.permission.ACCESS_COARSE_LOCATION"), ACCESS_FINE_LOCATION(
+                "android.permission.ACCESS_FINE_LOCATION"), CHANGE_WIFI_STATE("android.permission.CHANGE_WIFI_STATE"), ACCESS_WIFI_STATE("android.permission.ACCESS_WIFI_STATE");
+
+        private final String name;
+
+        Permissions(String name) {
+
+            this.name = name;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
